1cece080150d1f26b9b65d7c3787afdfc4231523
batuhangoren99@hotmail.com <batuhangoren99@hotmail.com>
2022-11-21 02:48:05 +0300

    Home component is done with responsive device mode

---
+++ src/components/home/Data.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Data = () => {
+    return (
+        <div className="home__data">
+            <h1 className="home__title">Batuhan Gören</h1>
+            <h3 className="home__subtitle">Front-End Developer</h3>
+            <p className="home__description">
+                I develop web application by using React.js. 
+                I'm very passionate and dedicated to my work. </p>
+
+            <a href="#contact" className="button button--flex">
+                Contact Me 
+                <i className="uil uil-message"></i>
+            </a>
+        </div>
+    )
+}
+
+export default Data
